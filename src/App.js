b293c2ea95f34d355341a45f87e554391fb42ecf
neutrino <neutrino.universe@gmail.com>
2022-10-15 11:17:48 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <>
